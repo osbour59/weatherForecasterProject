12bfdddfa40784724d3745a8a73d8108283adbbc
kylesonline <91856551+kylesonline@users.noreply.github.com>
2023-04-22 22:36:17 -0400

    Update userSchema.js

--- express_weather_server/models/userSchema.js
+++ express_weather_server/models/userSchema.js
@@ -1,2 +1,22 @@
 let mongoose = require('mongoose');
 
+
+const userSchema = new mongoose.Schema({
+    _id: {
+		type: String,
+		required: [true, 'You must enter a username.']
+},
+    displayName: String,
+    age: Number,
+    email:{
+		type: String,
+		required: [true, 'You must enter an email address.']
+	},
+	email_verified:{
+		type: Boolean,
+		default: false
+	}
+});
+
+const userCol=mongoose.model('User', userSchema)
+module.exports = userCol;
