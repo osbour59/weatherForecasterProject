03176e172b53f08f6383f0355417e200adf0aa4b
KillerNano224 <anthonyadass238@gmail.com>
2023-04-29 22:13:01 -0400

    logout test

--- express_weather_server/app.js
+++ express_weather_server/app.js
@@ -312,11 +312,19 @@ app.post('/addLocation', async (req, res) => {
   });
 
   //Logs the user out of the program
-  app.get('/logout', function (req, res) {
-    req.session.destroy();
-    res.send(`Logged out. <br><a href='/login'>Log back in`);
-  });
-  
+  app.delete('/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.status(400).send('Unable to log out')
+        } else {
+          res.send('Logout successful')
+        }
+      });
+    } else {
+      res.end()
+    }
+  })
 
 
  /* Starts the ExpressJS server on Port 6900 */
